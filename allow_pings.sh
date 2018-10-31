ced1875c4f3db1cdf7096d6bf6cc2a3e3270348e
David R. Piegdon <dgit@piegdon.de>
2018-10-31 01:28:45 +0000

    provide script to set sysctl such that vm can ping outside hosts

---
+++ allow_pings.sh
@@ -0,0 +1,6 @@
+#!/bin/sh +x
+# this command would allow the guest to send out pings,
+# even if it is running as a normal user.
+# NOTA BENE: RTFM. this actually allows ANY user on the hypervisor to
+#            send out pings, until the next reboot.
+/sbin/sysctl -w net.ipv4.ping_group_range='0 2147483647'
